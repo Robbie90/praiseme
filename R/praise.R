64b50bf9cb633cdb3fd1465d45f13409681d251b
Robbie90 <bonelli.r@mac2876.wehi.edu.au>
2018-11-21 15:59:09 +1100

    jgv

---
+++ R/praise.R
@@ -0,0 +1,22 @@
+#' @title Praise someone
+#' @export
+#' @param name Specifies recipient of name
+#' @examples
+#' praise("Anna")
+#' praise("Robbie")
+#'
+#' @description Let's dole out praise
+
+
+praise <-  function(name="Robbie"){
+
+  paste0("saskia loves ",name,"!")
+
+}
+
+
+praise("Roxy")
+
+
+
+
